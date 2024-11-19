16663ea12a9f3d9d6cb4740236592e70337c9e95
SuperWebMonkey <vikingsattack@outlook.com>
2024-11-19 15:59:22 -0800

    post request for product

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -47,6 +47,65 @@ router.get("/:id", async (req, res, next) => {
  * @description create a new product
  *
  */
-router.post("/", async (req, res, next) => {});
+router.post("/", async (req, res, next) => {
+  const { title, price, description, category } = req.body;
+
+  // Check if each key exists
+  if (!title || !description || !price || !category) {
+    return res.status(400).json({
+      message:
+        "All fields (title, description, price, category, stock) are required",
+    });
+  }
+
+  // Check for valid categories
+  const validCategories = ["electronics", "movies", "anime"];
+  if (!validCategories.includes(category)) {
+    return res.status(400).json({
+      message:
+        "Invalid category. Valid categories are: electronics, movies, anime",
+    });
+  }
+
+  try {
+    const newProduct = products({
+      title,
+      price,
+      description,
+      category,
+    });
+
+    await newProduct.save();
+
+    res.status(201).json({
+      message: "Product created successfully",
+      product: newProduct,
+    });
+  } catch (e) {
+    console.error("Error creating product:", e);
+    res
+      .status(500)
+      .json({ message: "Something went wrong. Product not created." });
+  }
+});
+
+/**
+ *
+ * Put /products
+ * @description modify a product
+ *
+ */
+router.put("/:id", async (req, res) => {
+  try {
+    const product = await products.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: "product not found" });
+    }
+    res.status(200).json(product);
+  } catch (e) {
+    res.status(500).json({ message: "Error fetching the product", error: err });
+  }
+});
 
 export default router;
